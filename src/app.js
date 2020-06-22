a3acc88966ca5941d07b6994a9bde24ea417a6c2
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-06-22 23:14:07 +0100

    Update todos array to take in objects

--- src/app.js
+++ src/app.js
@@ -4,12 +4,19 @@ document.addEventListener('DOMContentLoaded', () => {
     new Vue({
         el: "#app",
         data: {
-            todos: ["Buy shopping", "Clean bathroom", "Car's MOT"],
+            todos: [
+                {name: "Buy shopping", isPriority: false}, 
+                {name: "Clean bathroom", isPriority: false}, 
+                {name: "Car's MOT", isPriority: false}
+            ],
             newTodo: ""
         },
         methods: {
             saveNewTodo: function() {
-                this.todos.push(this.newTodo);
+                this.todos.push({
+                    name: this.newTodo,
+                    isPriority: false
+                });
                 this.newTodo = "";
             }
         }
